43d7083de6bcbe95119eafed3a12f99cf92a1167
Katiuska Alicea <katiuskaalicea@gmail.com>
2021-12-01 22:33:43 -0800

    Created the getRestaurants function in actions/index.js

--- template-with-redux/src/store/actions/index.js
+++ template-with-redux/src/store/actions/index.js
@@ -1,3 +1,19 @@
-const GET_RESTAURANTS = "GET_RESTAURANTS";
-const RES_SUCCESS = "RES_SUCCESS";
-const RES_FAIL = "RES_FAIL";
+import axios from "axios";
+
+export const GET_RESTAURANTS = "GET_RESTAURANTS";
+export const RES_SUCCESS = "RES_SUCCESS";
+export const RES_FAIL = "RES_FAIL";
+
+export const getRestaurants = () => (dispatch) => {
+  dispatch({ type: GET_RESTAURANTS });
+  axios
+    .get("https://s3.amazonaws.com/br-codingexams/restaurants.json")
+    .then((res) => {
+      console.log("actions: res: ", res);
+      dispatch({ type: RES_SUCCESS, payload: res.data.restaurants });
+    })
+    .catch((err) => {
+      console.log(err);
+      dispatch({ type: RES_FAIL, payload: err });
+    });
+};
