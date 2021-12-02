ab744fe61b4b2db38bf871afee510de19ade844e
Katiuska Alicea <katiuskaalicea@gmail.com>
2021-12-01 22:35:26 -0800

    Imported the action types to the reducer file and completed the reducer

--- template-with-redux/src/store/reducers/index.js
+++ template-with-redux/src/store/reducers/index.js
@@ -1,9 +1,37 @@
+import { GET_RESTAURANTS, RES_SUCCESS, RES_FAIL } from "../actions";
+
 const initialState = {
   restaurants: [],
   error: "",
   loading: false,
 };
 
-// export const restaurantsReducer = (state = initialState, action) => {
-//   return null;
-// };
+export const restaurantsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GET_RESTAURANTS: {
+      return {
+        ...state,
+        loading: true,
+      };
+    }
+    case RES_SUCCESS: {
+      return {
+        ...state,
+        restaurants: [...action.payload],
+        error: "",
+        loading: false,
+      };
+    }
+    case RES_FAIL: {
+      return {
+        ...state,
+        restaurants: [],
+        error: action.payload,
+        loading: false,
+      };
+    }
+    default: {
+      return state;
+    }
+  }
+};
