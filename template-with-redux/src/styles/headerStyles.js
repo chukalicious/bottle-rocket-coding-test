58157db0b1c32a9c4ddf94ea8502469226a44125
Katiuska Alicea <katiuskaalicea@gmail.com>
2021-12-02 18:36:40 -0800

    started on header styles

---
+++ template-with-redux/src/styles/headerStyles.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const HeaderDiv = styled.div`
+  width: 100%;
+  height 20vh; 
+  display: flex;
+  justify-content: space-around; 
+  align-items: flex-end; 
+  color: #fff;
+  background-color: #43e895;
+  border: 4px solid red;
+  margin-top: -1.5rem;
+`;
