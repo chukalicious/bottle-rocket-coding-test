3282986f5074b9d2b114385ec07b08bf85a1d34b
Katiuska Alicea <katiuskaalicea@gmail.com>
2021-12-05 18:59:38 -0800

    added navigation fucntionlity to the back and map icons in the Header.js

--- template-with-redux/src/components/common/Header.js
+++ template-with-redux/src/components/common/Header.js
@@ -1,16 +1,20 @@
+import { Link, useNavigate } from "react-router-dom";
 import iconmap from "../../assets/iconmap.png";
 import ic_webBack from "../../assets/ic_webBack.png";
 import { HeaderDiv, IconDiv, HeaderTitle } from "../../styles/headerStyles";
 
 const Header = () => {
+  const navigate = useNavigate();
+
   return (
     <HeaderDiv>
-      <IconDiv>
+      {/* needs to navigate back, not to home */}
+      <IconDiv onClick={(e) => navigate("/")}>
         {" "}
         <img src={ic_webBack} alt="back" />
       </IconDiv>
       <HeaderTitle>Lunch Tyme</HeaderTitle>
-      <IconDiv>
+      <IconDiv onClick={(e) => navigate("/map")}>
         <img src={iconmap} alt="map" />
       </IconDiv>
     </HeaderDiv>
